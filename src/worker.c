125a7608adf72f519f98ae0b638255d90e421295
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2020-06-02 20:45:24 +0300

    signal sigusr1

--- src/worker.c
+++ src/worker.c
@@ -19,12 +19,13 @@ volatile sig_atomic_t m_signal = 0;
 
 static int Worker_Init(int *w_fd, int *r_fd, char **buffer, const size_t bufferSize, ListPtr *list, HashTablePtr *h1, HashTablePtr *h2, struct sigaction **act);
 static string_nodePtr Worker_GetCountries(const int r_fd, char *buffer, const size_t bufferSize);
-static int Worker_Run(ListPtr list, HashTablePtr h1, HashTablePtr h2, const string_nodePtr countries, const int w_fd, const size_t bufferSize, const char *input_dir);
-static int Worker_wait_input(const int w_fd, const int r_fd, char *buffer, const size_t bufferSize, const ListPtr list, const HashTablePtr h1, const HashTablePtr h2);
+static int Worker_Run(ListPtr list, HashTablePtr h1, HashTablePtr h2, const string_nodePtr countries, string_nodePtr *dates, const int w_fd, const size_t bufferSize, const char *input_dir);
+static int Worker_wait_input(const int w_fd, const int r_fd, char *buffer, const size_t bufferSize, const ListPtr list, const HashTablePtr h1, const HashTablePtr h2, const string_nodePtr countries, string_nodePtr date, const char *input_dir);
 
 static void Worker_handleSignals(struct sigaction *act);
 static void handler(int signum);
 static void handle_sigint();
+static int handle_sigusr1(ListPtr list, HashTablePtr h1, HashTablePtr h2, const string_nodePtr countries, string_nodePtr dates, const char *input_dir);
 
 static int diseaseFrequency(const int w_fd, const size_t bufferSize, const char *str, const HashTablePtr ht);
 static int topk_AgeRanges(const int w_fd, const size_t bufferSize, const char *str, const ListPtr list);
@@ -39,8 +40,8 @@ int Worker(const size_t bufferSize, char *input_dir)
 {
     int r_fd = 0, w_fd = 0;
     char *buffer = NULL;
-    string_nodePtr countries = NULL;
     ListPtr list = NULL;
+    string_nodePtr countries = NULL, dates = NULL;
     HashTablePtr diseaseHT = NULL, countryHT = NULL;
 
     sigset_t blockset;
@@ -78,14 +79,12 @@ int Worker(const size_t bufferSize, char *input_dir)
         return -1;
     }
 
-    if (Worker_Run(list, diseaseHT, countryHT, countries, w_fd, bufferSize, input_dir) == -1)
+    if (Worker_Run(list, diseaseHT, countryHT, countries, &dates, w_fd, bufferSize, input_dir) == -1)
     {
         printf("Worker_Run() failed\n");
     }
 
-    clear_stringNode(countries);
-
-    if (Worker_wait_input(w_fd, r_fd, buffer, bufferSize, list, diseaseHT, countryHT) == -1)
+    if (Worker_wait_input(w_fd, r_fd, buffer, bufferSize, list, diseaseHT, countryHT, countries, dates, input_dir) == -1)
     {
         perror("Worker_wait_input() failed");
         return -1;
@@ -102,6 +101,8 @@ int Worker(const size_t bufferSize, char *input_dir)
     HashTable_Close(diseaseHT);
     HashTable_Close(countryHT);
     List_Close(list, F_PATIENT);
+    clear_stringNode(countries);
+    clear_stringNode(dates);
 
     return 0;
 }
@@ -203,6 +204,10 @@ static void handler(int signum)
     {
         m_signal = 1;
     }
+    else if (signum == SIGUSR1)
+    {
+        m_signal = 2;
+    }
 }
 
 static void handle_sigint()
@@ -223,7 +228,130 @@ static void handle_sigint()
     printf("worker: %d recieved siganl\n", getpid());
 }
 
-static int Worker_Run(ListPtr list, HashTablePtr h1, HashTablePtr h2, const string_nodePtr countries, const int w_fd, const size_t bufferSize, const char *input_dir)
+static int handle_sigusr1(ListPtr list, HashTablePtr h1, HashTablePtr h2, const string_nodePtr countries, string_nodePtr dates, const char *input_dir)
+{
+    wordexp_t p;
+    int flag = 1;
+    size_t len = 0;
+    DIR *dirp = NULL;
+    FILE *filePtr = NULL;
+    ListNodePtr node = NULL;
+    PatientPtr patient = NULL;
+    struct dirent *dir_info = NULL;
+    string_nodePtr country = countries, tmp = NULL;
+    char *path = NULL, *file = NULL, *line = NULL;
+
+    while (country != NULL)
+    {
+        if ((path = Worker_getPath(input_dir, country->str)) == NULL)
+        {
+            perror("Worker_getPath()");
+            return -1;
+        }
+
+        if ((dirp = opendir(path)) == NULL)
+        {
+            perror("opendir()");
+            return -1;
+        }
+
+        while ((dir_info = readdir(dirp)) != NULL)
+        {
+            if (!(strcmp(dir_info->d_name, ".") == 0 || strcmp(dir_info->d_name, "..") == 0))
+            {
+                if ((file = Worker_getPath(path, dir_info->d_name)) == NULL)
+                {
+                    perror("Worker_getPath()");
+                    return -1;
+                }
+
+                flag = 1;
+                tmp = dates;
+                while (tmp != NULL)
+                {
+                    if (!strcmp(tmp->str, file))
+                    {
+                        flag = 0;
+                    }
+                    tmp = tmp->next;
+                }
+
+                if (flag)
+                {
+                    if ((filePtr = fopen(file, "r")) == NULL)
+                    {
+                        perror("fopen()");
+                        return -1;
+                    }
+
+                    while (getline(&line, &len, filePtr) != -1)
+                    {
+                        strtok(line, "\n");
+                        wordexp(line, &p, 0);
+
+                        if (!strcmp(p.we_wordv[1], "ENTER"))
+                        {
+                            if (validatePatient(list, p.we_wordv[0]))
+                            {
+                                if ((patient = Patient_Init(p.we_wordv[0], p.we_wordv[2], p.we_wordv[3], p.we_wordv[5], p.we_wordv[4], country->str, dir_info->d_name)) == NULL)
+                                {
+                                    printf("Patient_Init() failed");
+                                    return -1;
+                                }
+                                patient->exitDate = NULL;
+
+                                if ((node = List_InsertSorted(list, patient)) == NULL)
+                                {
+                                    return -1;
+                                }
+
+                                if (HashTable_Insert(h1, patient->diseaseID, node) == -1)
+                                {
+                                    return -1;
+                                }
+
+                                if (HashTable_Insert(h2, patient->country, node) == -1)
+                                {
+                                    return -1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if ((patient = getPatient(&p, country->str, list)) == NULL)
+                            {
+                                // printf("Error: patient not registered\n");
+                            }
+                            else
+                            {
+                                if (Patient_addExitDate(patient, dir_info->d_name) == -1)
+                                {
+                                    printf("Patient_addExitDate() failed");
+                                }
+                            }
+                        }
+
+                        wordfree(&p);
+                    }
+
+                    fclose(filePtr);
+                    free(line);
+                }
+
+                free(file);
+            }
+        }
+
+        free(path);
+        closedir(dirp);
+
+        country = country->next;
+    }
+
+    return 0;
+}
+
+static int Worker_Run(ListPtr list, HashTablePtr h1, HashTablePtr h2, const string_nodePtr countries, string_nodePtr *dates, const int w_fd, const size_t bufferSize, const char *input_dir)
 {
     wordexp_t p;
     size_t len = 0;
@@ -264,6 +392,12 @@ static int Worker_Run(ListPtr list, HashTablePtr h1, HashTablePtr h2, const stri
                 if ((filePtr = fopen(file, "r")) == NULL)
                 {
                     perror("fopen()");
+                    return -1;
+                }
+
+                if ((*dates = add_stringNode(*dates, file)) == NULL)
+                {
+                    printf("add_stringNode() failed");
                     return -1;
                 }
 
@@ -348,7 +482,7 @@ static int Worker_Run(ListPtr list, HashTablePtr h1, HashTablePtr h2, const stri
     return 0;
 }
 
-static int Worker_wait_input(const int w_fd, const int r_fd, char *buffer, const size_t bufferSize, const ListPtr list, const HashTablePtr h1, const HashTablePtr h2)
+static int Worker_wait_input(const int w_fd, const int r_fd, char *buffer, const size_t bufferSize, const ListPtr list, const HashTablePtr h1, const HashTablePtr h2, const string_nodePtr countries, string_nodePtr dates, const char *input_dir)
 {
     fd_set rfds;
     wordexp_t p;
@@ -371,6 +505,10 @@ static int Worker_wait_input(const int w_fd, const int r_fd, char *buffer, const
                 {
                     handle_sigint();
                     break;
+                }
+                else if (m_signal == 2)
+                {
+                    handle_sigusr1(list, h1, h2, countries, dates, input_dir);
                 }
             }
             else
